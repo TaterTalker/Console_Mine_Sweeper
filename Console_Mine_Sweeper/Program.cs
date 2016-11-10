2f2806271dc3a6a30f6694b64c3628e1b63ed609
Tater Talker <sharkyxd7@gmail.com>
2016-11-10 10:43:39 -0500

    First commit

---
+++ Console_Mine_Sweeper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Console_Mine_Sweeper
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			while (true)
+			{
+				string a = Console.ReadLine();
+				if (a == "ayy")
+				{
+					Console.WriteLine("lmao");
+				}
+			}
+		}
+	}
+}
